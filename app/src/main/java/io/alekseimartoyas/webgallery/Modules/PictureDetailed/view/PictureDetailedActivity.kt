47ea904f40ebdaf25466ebc2dd26b3d8e8af538b
Алексей Мартояс Эдуардович <am.am664@gmail.com>
2018-05-03 12:18:11 +0300

    Initial commit

---
+++ app/src/main/java/io/alekseimartoyas/webgallery/Modules/PictureDetailed/view/PictureDetailedActivity.kt
@@ -0,0 +1,13 @@
+package io.alekseimartoyas.webgallery.Modules.PictureDetailed.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.alekseimartoyas.webgallery.R
+
+class PictureDetailedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture_detailed)
+    }
+}
---
+++ app/src/main/java/io/alekseimartoyas/webgallery/Modules/PicturesShowing/view/PicturesShowingActivity.kt
@@ -0,0 +1,13 @@
+package io.alekseimartoyas.webgallery.Modules.PicturesShowing.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.alekseimartoyas.webgallery.R
+
+class PicturesShowingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pictures_showing)
+    }
+}
